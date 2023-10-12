ac5b74e14cf54f8e0398beb43ac452538e4fbb55
emanuel <emanuelssobral@gmail.com>
2023-10-12 19:27:29 -0300

    teste calculadora final

--- 02 - SobrecargaDeMetodos/CalculadoraCientifica/src/TesteCalculadora.java
+++ 02 - SobrecargaDeMetodos/CalculadoraCientifica/src/TesteCalculadora.java
@@ -11,37 +11,57 @@ public class TesteCalculadora {
 
         switch (operacao) {
             case "1":
-                System.out.println("Digite a base:");
-                byte baseByte = scanner.nextByte();
-                System.out.println("Digite o expoente:");
-                byte expoenteByte = scanner.nextByte();
-                System.out.println("Resultado: " + calculadora.potencia(baseByte, expoenteByte));
+                System.out.println("Escolha os tipos de input da potencia \n (1 para byte, 2 para inteiros, 3 para doubles):");
+                String potenciaEscolha = scanner.nextLine();
 
-                System.out.println("Digite um número inteiro:");
-                int baseInt = scanner.nextInt();
-                System.out.println("Digite um número decimal:");
-                double expoenteDouble = scanner.nextDouble();
-                System.out.println("Resultado: " + calculadora.potencia(baseInt, expoenteDouble));
-
-                System.out.println("Digite um número decimal:");
-                String baseStr = scanner.next();
-                System.out.println("Digite um número decimal:");
-                String expoenteStr = scanner.next();
-                System.out.println("Resultado: " + calculadora.potencia(baseStr, expoenteStr));
-                break;
+                switch (potenciaEscolha) {
+                    case "1":
+                        System.out.println("Digite a base:");
+                        byte baseByte = scanner.nextByte();
+                        System.out.println("Digite o expoente:");
+                        byte expoenteByte = scanner.nextByte();
+                        System.out.println("Resultado: " + calculadora.potencia(baseByte, expoenteByte));
+                        break;
+                    case "2":
+                        System.out.println("Digite um número inteiro:");
+                        int baseInt = scanner.nextInt();
+                        System.out.println("Digite um número decimal:");
+                        double expoenteDouble = scanner.nextDouble();
+                        System.out.println("Resultado: " + calculadora.potencia(baseInt, expoenteDouble));
+                        break;
+                    case "3":
+                        System.out.println("Digite um número decimal:");
+                        String baseStr = scanner.next();
+                        System.out.println("Digite um número decimal:");
+                        String expoenteStr = scanner.next();
+                        System.out.println("Resultado: " + calculadora.potencia(baseStr, expoenteStr));
+                        break;
+                    default:
+                        System.out.println("Operação inválida.");
+                    }
             case "2":
-                System.out.println("Digite um número inteiro:");
-                int numeroInt = scanner.nextInt();
-                System.out.println("Resultado: " + calculadora.raiz(numeroInt));
-
-                System.out.println("Digite um número decimal:");
-                double numeroDouble = scanner.nextDouble();
-                System.out.println("Resultado: " + calculadora.raiz(numeroDouble));
-
-                System.out.println("Digite um número decimal:");
-                String numeroStr = scanner.next();
-                System.out.println("Resultado: " + calculadora.raiz(numeroStr));
-                break;
+                System.out.println("Escolha os tipos de input da raiz \n (1 para inteiro, 2 para double, 3 para inteiroSTR):");
+                String raizEscolha = scanner.nextLine();
+                
+                switch (raizEscolha) {
+                    case "1":
+                        System.out.println("Digite um número inteiro:");
+                        int numeroInt = scanner.nextInt();
+                        System.out.println("Resultado: " + calculadora.raiz(numeroInt));
+                        break;
+                    case "2":
+                        System.out.println("Digite um número decimal:");
+                        double numeroDouble = scanner.nextDouble();
+                        System.out.println("Resultado: " + calculadora.raiz(numeroDouble));
+                        break;
+                    case "3":
+                        System.out.println("Digite um número decimal:");
+                        String numeroStr = scanner.next();
+                        System.out.println("Resultado: " + calculadora.raiz(numeroStr));
+                        break;
+                    default:
+                        System.out.println("Operação inválida.");
+                }
             default:
                 System.out.println("Operação inválida.");
         }
