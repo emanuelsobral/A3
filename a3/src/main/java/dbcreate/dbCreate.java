bf6d57e0344368544dfd44df4caf939973c75551
Emanuel <82630009+emanuelsobral@users.noreply.github.com>
2023-10-26 10:51:38 -0300

    dbcreate automatic

---
+++ a3/src/main/java/dbcreate/dbCreate.java
@@ -0,0 +1,59 @@
+package dbcreate;
+
+import java.sql.*;
+
+public class dbCreate {
+
+    public static void main(String[] args) {
+        String nomeBanco = "a3";
+        String nomeTabela = "usuario";
+
+        //conexao
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String password = "usjt";
+
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão estabelecida com sucesso!");
+
+            // Criar um objeto Statement para executar comandos SQL
+            Statement stmt = con.createStatement();
+
+            String sql = "CREATE DATABASE " + nomeBanco;
+
+            int result = stmt.executeUpdate(sql);
+            if (result == 1) {
+                System.out.println("Banco de dados " + nomeBanco + " criado com sucesso!");
+            } else {
+                System.out.println("Erro ao criar o banco de dados " + nomeBanco);
+            }
+
+            url += nomeBanco;
+
+            // Reconectar ao MySQL com a nova URL
+            con = DriverManager.getConnection(url, user, password);
+
+            // Criar um novo objeto Statement para executar comandos SQL no novo banco de dados
+            stmt = con.createStatement();
+
+            sql = "CREATE TABLE " + nomeTabela + " (userID INT NOT NULL AUTO_INCREMENT, nome VARCHAR(60) NOT NULL, email VARCHAR(60) NOT NULL, senha VARCHAR(60) NOT NULL, altura FLOAT NOT NULL, idade INT(11) NOT NULL, peso FLOAT NOT NULL, frequencia INT(2) NOT NULL, genero TINYTEXT NOT NULL, PRIMARY KEY (userID))";
+
+            result = stmt.executeUpdate(sql);
+            if (result == 0) {
+                System.out.println("Tabela " + nomeTabela + " criada com sucesso!");
+                stmt.executeUpdate("INSERT INTO usuario (nome, email, senha, altura, idade, peso, frequencia, genero) VALUES ('Maria', 'maria@email.com', 'maria123', 1.62, 28, 54.2, 2, 'F')");
+                stmt.executeUpdate("INSERT INTO usuario (nome, email, senha, altura, idade, peso, frequencia, genero) VALUES ('Roberto', 'roberto@email.com', 'roberto34', 1.72, 43, 72.2, 5, 'M')");
+            } else {
+                System.out.println("Erro ao criar a tabela " + nomeTabela);
+            }
+
+            stmt.close();
+            con.close();
+
+        } catch (SQLException e) {
+            // Tratar os possíveis erros de conexão ou de execução do comando SQL
+            e.printStackTrace();
+        }
+    }
+}
