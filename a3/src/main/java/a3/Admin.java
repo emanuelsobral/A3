357188f16137ea20195ed8fd30ceb7a515a952f0
emanuel <emanuelssobral@gmail.com>
2023-11-01 15:34:32 -0300

    changes

--- a3/src/main/java/a3/Admin.java
+++ a3/src/main/java/a3/Admin.java
@@ -1,6 +1,7 @@
 package a3;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Admin extends Usuario {
     public Admin(int id, String nome, float altura, int idade, float peso, int frequencia, String genero) {
@@ -23,7 +24,73 @@ public class Admin extends Usuario {
         // implementar a lógica para deletar um usuário
     }
 
-    public void alterarUsuario(Connection connection) {
-        // implementar a lógica para alterar as informações de um usuário
+    public void alterarUsuario(Connection connection, Scanner sc) throws SQLException {
+        System.out.println("Voce escolheu alterar informacoes de um usuario.");
+        System.out.print("Digite o ID do usuario que voce quer alterar: ");
+        int id = sc.nextInt();
+        sc.nextLine(); // consumir a quebra de linha
+        String sql = "SELECT * FROM usuario WHERE userID = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    System.out.println("Usuario encontrado.");
+                    int opcaoAlterar;
+                    do {
+                        System.out.println("Escolha qual informacao voce quer alterar: ");
+                        System.out.println("1 - Nome");
+                        System.out.println("2 - Email");
+                        System.out.println("3 - Senha");
+                        System.out.println("4 - Altura");
+                        System.out.println("5 - Idade");
+                        System.out.println("6 - Peso");
+                        System.out.println("7 - Frequencia");
+                        System.out.println("8 - Genero");
+                        System.out.println("9 - Admin");
+                        System.out.println("0 - Sair");
+                        opcaoAlterar = sc.nextInt();
+                        sc.nextLine(); // consumir a quebra de linha
+                        switch (opcaoAlterar) {
+                            case 1:
+                                alterarNome(connection, sc, id);
+                                break;
+                            case 2:
+                                alterarEmail(connection, sc, id);
+                                break;
+                            case 3:
+                                alterarSenha(connection, sc, id);
+                                break;
+                            case 4:
+                                alterarAltura(connection, sc, id);
+                                break;
+                            case 5:
+                                alterarIdade(connection, sc, id);
+                                break;
+                            case 6:
+                                alterarPeso(connection, sc, id);
+                                break;
+                            case 7:
+                                alterarFrequencia(connection, sc, id);
+                                break;
+                            case 8:
+                                alterarGenero(connection, sc, id);
+                                break;
+                            case 9:
+                                alterarAdmin(connection, sc, id);
+                                break;
+                            case 0:
+                                System.out.println("Saindo do modo de alteracao.");
+                                break;
+                            default:
+                                System.out.println("Opcao invalida.");
+                                break;
+                        }
+                    } while (opcaoAlterar != 0);
+                } else {
+                    System.out.println("Nao existe usuario com esse ID.");
+                }
+            }
+        }
+    }
     }
 }
