57d0391ac18a3f2e0ad9cbbd612d5081d614d325
Amandiques <145459226+Amandiques@users.noreply.github.com>
2023-11-21 11:45:15 -0300

    Update Login.java (correção)

--- a3/src/main/java/a3/Login.java
+++ a3/src/main/java/a3/Login.java
@@ -1,45 +1,42 @@
 package a3;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.*;
 
-public class Delete{
+public class Login {
+    private String email;
+    private String senha;
 
-    Conexao con = new Conexao("root", "usjt");
-
-    public static void deletarUsuario(Connection connection, Scanner sc, Usuario usuario) throws SQLException {
-        if (usuario == null) {
-            System.out.println("Você precisa estar logado para deletar um usuário.");
-            return;
-        }
-
-        if (usuario instanceof Admin) {
-            System.out.print("Digite o id do usuário que deseja deletar: ");
-            int id = sc.nextInt();
-            sc.nextLine(); // consumir a quebra de linha
-       //     deletarUsuario(connection, id); teacher não soube dizer oq é isso, perguntou pra ti emanu 
-        } else {
-            System.out.print("Tem certeza que deseja deletar sua conta? (S ou N): ");
-            String resposta = sc.nextLine();
-            if (resposta.equals("S") || resposta.equals("s")) {
-              //  deletarUsuario(connection, usuario.); método get id não existe querido
-            } else {
-                System.out.println("Operação cancelada.");
-            }
-        }
+    public Login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
     }
 
-    private static void deletarUsuario(Connection connection, Usuario usuario) throws SQLException {
-        String sql = "DELETE FROM usuario WHERE id = ?";
+    // getters e setters para cada campo
+
+    public Usuario fazerLogin(Connection connection) throws SQLException {
+        String sql = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, usuario.id);
-            int linhasAfetadas = statement.executeUpdate();
-            if (linhasAfetadas > 0) {
-                System.out.println("Usuário deletado com sucesso.");
-            } else {
-                System.out.println("Erro ao deletar usuário.");
+            statement.setString(1, this.email);
+            statement.setString(2, this.senha);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("userID");
+                    String nome = resultSet.getString("nome");
+                    float altura = resultSet.getFloat("altura");
+                    int idade = resultSet.getInt("idade");
+                    float peso = resultSet.getFloat("peso");
+                    int frequencia = resultSet.getInt("frequencia");
+                    String genero = resultSet.getString("genero");
+                    boolean admin = resultSet.getBoolean("admin");
+
+                    if (admin) {
+                        return new Admin(id, nome, altura, idade, peso, frequencia, genero);
+                    } else {
+                        return new Usuario(id, nome, altura, idade, peso, frequencia, genero, false);
+                    }
+                } else {
+                    return null;
+                }
             }
         }
     }
