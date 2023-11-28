f2371cb6b4aae01546161769d0e68df87186c433
Emanuel <82630009+emanuelsobral@users.noreply.github.com>
2023-11-28 15:47:46 -0300

    better ui

--- a3/src/main/java/a3/Cadastro.java
+++ a3/src/main/java/a3/Cadastro.java
@@ -5,43 +5,45 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import javax.swing.*;
+import javax.swing.JComboBox;
 
 public class Cadastro {
 
     Conexao con = new Conexao("root", "RootAdmin123");
 
-    public static void fazerCadastro(Connection connection) throws SQLException {
-        JTextField nomeField = new JTextField();
-        JTextField emailField = new JTextField();
-        JPasswordField senhaField = new JPasswordField();
-        JTextField alturaField = new JTextField();
-        JTextField idadeField = new JTextField();
-        JTextField pesoField = new JTextField();
-        JTextField generoField = new JTextField();
+        public static void fazerCadastro(Connection connection) throws SQLException {
+            JTextField nomeField = new JTextField();
+            JTextField emailField = new JTextField();
+            JPasswordField senhaField = new JPasswordField();
+            JTextField alturaField = new JTextField();
+            JTextField idadeField = new JTextField();
+            JTextField pesoField = new JTextField();
+            JComboBox<String> generoField = new JComboBox<>(new String[]{"M", "F"});
 
-        Object[] fields = {
-                "Nome:", nomeField,
-                "Email:", emailField,
-                "Senha:", senhaField,
-                "Altura (metros):", alturaField,
-                "Idade (anos):", idadeField,
-                "Peso (quilos):", pesoField,
-                "Gênero (M ou F):", generoField
-        };
+            Object[] fields = {
+                    "Nome:", nomeField,
+                    "Email:", emailField,
+                    "Senha:", senhaField,
+                    "Altura (metros):", alturaField,
+                    "Idade (anos):", idadeField,
+                    "Peso (quilos):", pesoField,
+                    "Gênero:", generoField
+            };
 
-        int option = JOptionPane.showConfirmDialog(null, fields, "Cadastro", JOptionPane.OK_CANCEL_OPTION);
-        if (option == JOptionPane.OK_OPTION) {
-            String nome = nomeField.getText();
-            String email = emailField.getText();
-            String senha = new String(senhaField.getPassword());
-            float altura = Float.parseFloat(alturaField.getText());
-            int idade = Integer.parseInt(idadeField.getText());
-            float peso = Float.parseFloat(pesoField.getText());
-            String genero = generoField.getText();
+            int option = JOptionPane.showConfirmDialog(null, fields, "Cadastro", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
+                String nome = nomeField.getText();
+                String email = emailField.getText();
+                String senha = new String(senhaField.getPassword());
+                float altura = Float.parseFloat(alturaField.getText());
+                int idade = Integer.parseInt(idadeField.getText());
+                float peso = Float.parseFloat(pesoField.getText());
+                String genero = (String) generoField.getSelectedItem();
 
-            cadastrarUsuario(connection, nome, email, senha, altura, idade, peso, genero);
+                cadastrarUsuario(connection, nome, email, senha, altura, idade, peso, genero);
+            }
         }
-    }
+
 
     private static void cadastrarUsuario(Connection connection, String nome, String email, String senha, float altura,
             int idade, float peso, String genero) throws SQLException {
